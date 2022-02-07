5bbdd401ead3f5b36beec438eed42cbf93970643
Anthony Osei <A.A.OSEI-2020@hull.ac.uk>
2022-02-07 01:58:02 +0000

    Add project files.

---
+++ Temperature/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//fahrenheit measurement
+int main(int argc, char** argv) {
+	
+	float fahrenheit =0.0f;  // declaration of variables
+	float celsius = 0.0f;
+	cout << "input a Fahrenheit measurement\n" << endl;
+	cin >> fahrenheit;
+
+	
+}
