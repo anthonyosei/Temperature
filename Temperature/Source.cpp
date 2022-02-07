b33b5be293d90aa272bfb00d889f7285bdebb816
Anthony Osei <A.A.OSEI-2020@hull.ac.uk>
2022-02-07 18:34:30 +0000

    converting to celsuis

--- Temperature/Source.cpp
+++ Temperature/Source.cpp
@@ -8,5 +8,10 @@ int main(int argc, char** argv) {
 	cout << "input a Fahrenheit measurement\n" << endl;
 	cin >> fahrenheit;
 
-	
+	//convert to celsius
+
+	celsius = (float) 5 / 9 * (fahrenheit - 32);
+	cout << fahrenheit << " Your value for celsuis is " << celsius << " centigradi " << endl;
+
+	return 0;
 }
