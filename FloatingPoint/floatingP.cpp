45035bfc3837ad51c64136b419ff29d92b554cdd
Anthony Osei <A.A.OSEI-2020@hull.ac.uk>
2022-02-17 16:05:51 +0000

    Changed Y variable

--- FloatingPoint/floatingP.cpp
+++ FloatingPoint/floatingP.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	double x = 10.0;
-	double y = 10.0;
+	double y = 20.0 / 2.0;
 
-	if (x == y) // 
+	if (x == y) // work on this
 		cout << "X and Y are identical" << endl;
 }
